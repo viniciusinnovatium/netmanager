b6eda05cddca0105ea4de81ffe922e169d3c2043
vinicius <vinicius@innovatium.com.br>
2014-06-09 15:27:36 -0300

    Criacao de servlet para testar a persistencia dos registros da aplicacao.

---
+++ src/main/java/com/disclinc/netmanager/servlet/TestePersistenciaServlet.java
@@ -0,0 +1,31 @@
+package com.disclinc.netmanager.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mLibrary.mContext;
+import mLibrary.mNMObject;
+
+/**
+ * Servlet implementation class TestePersistenciaServlet
+ */
+public class TestePersistenciaServlet extends NetmanagerServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	void doHandleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		mContext m$ = new mContext(response);
+		mNMObject obj = new mNMObject();
+		String record = obj.loadRecord(m$, "MEDPatient", "0||85");
+		m$.Cmd.Write("\n"+record);
+		
+		obj.saveRecord(m$, "MEDPatient", "0||-1", record);
+		record = obj.loadRecord(m$, "MEDPatient", "0||-1");
+		m$.Cmd.Write("\n"+record);
+	}
+
+}
